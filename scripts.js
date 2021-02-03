6cbde5d59155001b54b043bee853ec3f904824d2
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-02-03 00:29:59 -0300

    Salvando o ultimo tema

--- scripts.js
+++ scripts.js
@@ -283,4 +283,36 @@ checkbox.addEventListener("change", ({ target }) => {
 })
 
 
+const isExistLocalStorage = (key) => 
+  localStorage.getItem(key) != null
+
+const createOrEditLocalStorage = (key, value) => 
+  localStorage.setItem(key, JSON.stringify(value))
+
+const getValeuLocalStorage = (key) =>
+  JSON.parse(localStorage.getItem(key))
+
+checkbox.addEventListener("change", ({target}) => {
+  if (target.checked) {
+    changeColors(darkMode) 
+    createOrEditLocalStorage('modo','darkMode')
+  } else {
+    changeColors(initialColors)
+    createOrEditLocalStorage('modo','initialColors')
+  }
+})
+
+if(!isExistLocalStorage('modo'))
+  createOrEditLocalStorage('modo', 'initialColors')
+
+
+if (getValeuLocalStorage('modo') === "initialColors") {
+  checkbox.removeAttribute('checked')
+  changeColors(initialColors);
+} else {
+  checkbox.setAttribute('checked', "")
+  changeColors(darkMode);
+}
+
+
 App.init()
